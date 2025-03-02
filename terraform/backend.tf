d89ad2c4044f37f0e6ee32ffbc1506487f58d4a4
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-03-02 19:48:31 +0530

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "statefilebackup99" # Replace with your bucket name
+    key            = "calculatorAngular/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
